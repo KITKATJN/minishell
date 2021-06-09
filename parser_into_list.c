35de83167cf78a884fd61ef2afa442e4ea90a74e
Ilya <lvutinilyha@gmail.com>
2021-06-09 12:34:26 +0300

    исправил ошибку вывода $ и отправки пустой команды в bsopia

--- parser_into_list.c
+++ parser_into_list.c
@@ -165,6 +165,8 @@ t_command	*parser_into_list(char *str1, t_untils *untils)
 		{
 			if (current->special_array[i_env] == 5)
 			{
+				if (current->special_array[i_env + 1] != 0)
+				{
 				env_tmp = malloc(sizeof(char*) * (ft_strlen(current->symbol) - i_env) + 1);
 				ft_bzero(env_tmp, ft_strlen(current->symbol) - i_env + 1);
 				j_env = i_env + 1;
@@ -189,6 +191,7 @@ t_command	*parser_into_list(char *str1, t_untils *untils)
 				ft_bzero(string_before_doolar, ft_strlen(current->symbol) + 1);
 				i_for_str_before_dollar = 0;
 				continue ;
+				}
 			}
 			if (i_env < ft_strlen(current->symbol))
 				string_before_doolar[i_for_str_before_dollar] = current->symbol[i_env];
@@ -215,8 +218,11 @@ t_command	*parser_into_list(char *str1, t_untils *untils)
 		ft_lstadd_back_parser(&commands, ft_lstnew_parser(ft_strdup(current->symbol), 0));
 		current = current->next;
 	}
-	bsopia_func(commands, 0, untils);
-	ft_lstclear_command(&commands);
+	if (commands != 0)
+	{
+		bsopia_func(commands, 0, untils);
+		ft_lstclear_command(&commands);
+	}
 	ft_lstclear_parser2(&list_of_command);
 	return (commands);
 }
