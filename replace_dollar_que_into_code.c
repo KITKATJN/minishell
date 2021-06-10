d3d4549a15d485a67a2fe00b0c35cdf3e34766ec
Constantine Marguerita <cmarguer@il-f5.msk.21-school.ru>
2021-06-10 14:49:14 +0300

    функция для подстановки &?

---
+++ replace_dollar_que_into_code.c
@@ -0,0 +1,26 @@
+#include "minishell.h"
+
+char        *replace_dollar_que_into_code(char *str, t_untils *untils)
+{
+    char    *start;
+    char    *temporary;
+    char    *first_part;
+    char    *second_part;
+
+    if (str == 0)
+        return (str);
+    while (1)
+    {
+        if ((start = ft_strnstr(str, "$?+?", ft_strlen(str))) == 0)
+            return (str);
+        first_part = ft_substr(str, 0, ft_strlen(str) - ft_strlen(start));
+        second_part = ft_substr(str, ft_strlen(str) - ft_strlen(start) + 4, ft_strlen(str));
+        temporary = ft_strjoin(first_part, ft_itoa(untils->status));
+        ft_free(first_part);
+        ft_free(str);
+        str = ft_strjoin(temporary, second_part);
+        ft_free(temporary);
+        ft_free(second_part);
+    }
+    return (str);
+}
