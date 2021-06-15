1c67f1eac59979cff11722c9eacfeb85db6cf539
Constantine Marguerita <cmarguer@il-i5.msk.21-school.ru>
2021-06-15 16:14:59 +0300

    norminette

--- remove_paired_quotes.c
+++ remove_paired_quotes.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-t_parser *remove_paired_quotes(t_parser	*start)
+t_parser	*remove_paired_quotes(t_parser	*start)
 {
 	t_parser	*tmp;
 	t_parser	*tmp2;
@@ -18,7 +18,7 @@ t_parser *remove_paired_quotes(t_parser	*start)
 			tmp3 = current;
 			while (tmp3)
 			{
-				if(tmp3->symbol[0] == '\"')
+				if (tmp3->symbol[0] == '\"')
 					num_quotes++;
 				tmp3 = tmp3->back;
 			}
@@ -31,16 +31,17 @@ t_parser *remove_paired_quotes(t_parser	*start)
 				current = tmp2;
 			}
 		}
-		if (current->symbol[0] == '\"' && current->next->symbol[0] == '\"' && current->back != 0 && current->back->special != 3)
+		if (current->symbol[0] == '\"' && current->next->symbol[0]
+			== '\"' && current->back != 0 && current->back->special != 3)
 		{
 			tmp3 = current;
 			while (tmp3)
 			{
-				if(tmp3->symbol[0] == '\'')
+				if (tmp3->symbol[0] == '\'')
 					num_quotes++;
 				tmp3 = tmp3->back;
 			}
-			if (num_quotes % 2 == 0)// && num_quotes != 0)
+			if (num_quotes % 2 == 0)
 			{
 				tmp = current->next;
 				tmp2 = current->back;
